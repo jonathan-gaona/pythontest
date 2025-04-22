2bf3afccdbde43f3d5bfd39a4ed54113f1973c5d
jonathan-gaona <gaona2057@gmail.com>
2025-04-22 10:30:18 -0400

    2 python

--- hola.py
+++ hola.py
@@ -1,4 +1,6 @@
 
+# suma 2 numeros
+
 # variables 
 # nombre="joni"
 # edad=25
@@ -15,7 +17,36 @@
 # print("ingrese su nombre")
 # nombre=input()
 
-print ("ingrese 2 numeros")
-n1=int(input())
-n2=int(input())
-print ("el resultado de la suma es ",n1+n2)
+# print ("ingrese 2 numeros")
+# n1=int(input())
+# n2=int(input())
+# print ("el resultado de la suma es ",n1+n2)
+
+# promedio:
+
+
+
+# print ("ingrese 3 notas")
+# n1=float(input()) 
+# n2=float(input())
+# n3=float(input())
+# total=(n1+n2+n3)/3
+# print("su promedio es", round(total))
+# if total>=4: 
+#     print("usted abrobó")
+# else: 
+#     print("usted reprobó")
+
+# verificar la edad
+
+# print("ingrese su edad")
+# edad=int(input())
+
+# if edad<12:
+#     print("usted es un niño")
+# elif edad>=12 and edad<=17:
+#     print("usted es adolecente")
+# elif edad>=18 and edad<=64:
+#     print("usted es adulto")    
+# else: 
+#     print("usted es un adulto mayor")        
