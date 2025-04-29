d63f5f9fd7993711736756b86f3267376683a436
jonathan-gaona <gaona2057@gmail.com>
2025-04-28 20:58:03 -0400

    02 py

--- 12-while.py
+++ 12-while.py
@@ -1,14 +1,14 @@
 lenguajes = ["python", "rubi", "php", "javascrip", "java"]
 
-# i = 1
-# while i <=5:
-#     print(i)
-#     i = i + 1
+i = 1
+while i <=5:
+    print(i)
+    i = i + 1
  
-# i = 1
-# while i <= 5:
-#     print(i * "joni ")
-#     i += 1
+i = 1
+while i <= 5:
+    print(i * "joni ")
+    i += 1
 
 i = 0
 
---
+++ 13-for.py
@@ -0,0 +1,11 @@
+lenguajes = ["python", "rubi", "php", "javascrip", "java"]
+
+for lenguaje in lenguajes:
+    print(lenguaje)
+
+#     i = 0
+
+# while i < len(lenguajes):
+#     print(lenguajes[i])
+#     i = i + 1
+
