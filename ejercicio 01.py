49b6a6bfb77d62623e49b6091ef3ea4f48cf329a
jonathan-gaona <gaona2057@gmail.com>
2025-04-29 10:22:30 -0400

    guia 1

---
+++ ejercicio 01.py
@@ -0,0 +1,13 @@
+resultado= int(input("ingrese un numero"))
+
+
+i = 1
+while i !=resultado:
+    print(i)
+    i = i + 1
+
+if  resultado % == 0:
+    pares += 1
+    print("los pares son: ", pares)
+
+     
